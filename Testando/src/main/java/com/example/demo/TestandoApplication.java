7e83953a9dc481018dc9244f8c2bc48ee023efb1
Lucas Sales Salvo Petruci <99514230+LucasPetruci@users.noreply.github.com>
2023-06-20 19:14:04 -0300

    ...

--- Testando/src/main/java/com/example/demo/TestandoApplication.java
+++ Testando/src/main/java/com/example/demo/TestandoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestandoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestandoApplication.class, args);
+		SpringApplication.run(TestandoApplication.class, args);//Vamos ver se sai
 	}
 
 }
